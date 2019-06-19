8d24a12d9879bdfa7d04c1344a3838cae32477e3
krystek77 <krystian.wrona77@gmail.com>
2019-06-19 13:39:14 +0200

    correct handle errors in loadUser action

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -3,16 +3,16 @@ const config = require('config');
 
 function auth(req, res, next) {
 	const token = req.header('x-auth-token');
-
-	if (!token)
+	if (!token) {
 		return res.status(401).json({
 			message: 'No token, authorization denied',
 			status: '401',
 		});
+	}
 	try {
 		const decoded = jwt.verify(token, config.get('jwtSecret'));
 		req.user = decoded;
-		console.log("DECODED TOKEN",decoded);
+		console.log('DECODED TOKEN', decoded);
 		next();
 	} catch (e) {
 		res.status(400).json({ message: 'Token is not valid' });
--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 	next(error);
 });
 app.use((error, req, res, next) => {
-	res.status(error.status || 500);
+	res.status(error.status || 500 || 401);
 	res.json({ message: error.message, status: error.status });
 });
 module.exports = app;
